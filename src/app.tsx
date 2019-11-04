67a39297a7f285094b25256d67e51ac01036e98e
Nate Kidwell <nate@ludicast.com>
2019-11-04 02:35:51 -0500

    adding context for search

--- src/app.tsx
+++ src/app.tsx
@@ -5,13 +5,17 @@ import {
   Switch
 } from "react-router-dom";
 import { SearchPage } from './components/searchPage';
+import { SearchProvider } from './searchContext';
+
 import { FavoritesPage } from './components/favoritesPage';
 
 export const App = () => {
     return (<Router>
-        <Switch>
-            <Route exact path="/" component={SearchPage} />
-            <Route path="/favorites" component={FavoritesPage} />
-        </Switch>
+        <SearchProvider>
+            <Switch>
+                <Route exact path="/" component={SearchPage} />
+                <Route path="/favorites" component={FavoritesPage} />
+            </Switch>
+        </SearchProvider>
     </Router>);
 };
--- src/components/searchPage.tsx
+++ src/components/searchPage.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import ReactDOM from 'react-dom'
 import { useSelector, useDispatch } from "react-redux";
 import { currentGemList, areGemsLoading } from '../store/selectors';
 import { fetchGemsAsync } from '../store/actions';
 import { GemCard } from './gemCard';
 import { Gem } from '../models';
+import { SearchContext } from '../searchContext';
 
 const GemGrid = ({gems}: {gems: Gem[]}) => {
     return <ul>
@@ -20,15 +21,15 @@ export const SearchPage = () => {
     const gemList = useSelector(currentGemList);
     const loadingList = useSelector(areGemsLoading);
     const dispatch = useDispatch();
-    const [query, setQuery] = useState("");
+    const {query, setQuery} = useContext(SearchContext);
 
-    const search = () => {
-        dispatch(fetchGemsAsync.request(query))
+    const startSearch = () => {
+        dispatch(fetchGemsAsync.request(query));
     };
 
-    const starterSearch = (query: string) => {
-        setQuery(query);
-        search();
+    const starterSearch = (value: string) => () => {
+        setQuery(value);
+        startSearch();
     };
 
     return <>
@@ -37,8 +38,8 @@ export const SearchPage = () => {
             placeholder="Search Gem"
             onChange={evt => setQuery(evt.target.value)}
         />
-        <button disabled={query===""} onClick={search}>search</button>
-        
+        <button disabled={query===""} onClick={startSearch}>search</button>
+
         <b>{loadingList ? "LOADING" : ""}</b>
         {gemList.length > 0 ? <GemGrid gems={gemList}></GemGrid>
         : query.length === 0  &&  <p>Load.  Some suggestions: <b onClick={starterSearch('rspec')}>rspec</b>  </p> }
