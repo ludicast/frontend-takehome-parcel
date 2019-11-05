d8611ff21350a76f499b4d029bd4a672166a8c7b
Nate Kidwell <nate@ludicast.com>
2019-11-05 05:14:04 -0500

    use hero

--- src/components/searchPage.tsx
+++ src/components/searchPage.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useContext } from "react";
 import { useSelector } from "react-redux";
 
-import { currentGemList } from '../store/selectors';
-import { GemGrid } from './gemGrid';
+import { currentGemList } from "../store/selectors";
+import { GemGrid } from "./gemGrid";
+import { SearchContext } from "../searchContext";
+import { EmptyHero } from "./emptyHero";
 
 export const SearchPage = () => {
-    const gemList = useSelector(currentGemList);
+  const gemList = useSelector(currentGemList);
+  const { query } = useContext(SearchContext);
 
-    return <>
-        <GemGrid gems={gemList}></GemGrid>
+  return query ? (
+    <>
+      <div>{query}</div>
+      <GemGrid gems={gemList}></GemGrid>
     </>
-}
+  ) : (
+    <EmptyHero msg="Enter Search Term"></EmptyHero>
+  );
+};
