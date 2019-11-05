c79b5b860bcdd9364f0822a92401cb220c9aed07
Nate Kidwell <nate@ludicast.com>
2019-11-05 11:17:23 -0500

    Add some styling

--- src/components/gemCard.tsx
+++ src/components/gemCard.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import { Heart } from './heart';
+import React from "react";
+import { Heart } from "./heart";
 import { Gem } from "../models";
-import { useStyles } from './classes';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { useStyles } from "./classes";
+import {
+  Card,
+  CardContent,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Typography
+} from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 interface GemCardProps {
-    gem: Gem
+  gem: Gem;
 }
 
-export const GemCard = ({gem}: GemCardProps) => {
-    const classes = useStyles();
-    return <Card className={classes.card}>
-        <CardContent className={classes.cardContent}>
-            <Typography variant="h5" component="h5">
-                <Heart gem={gem}></Heart>
-                {gem.name}
-            </Typography>
-            <Typography>
-                Authors: { gem.authors }
-            </Typography>
-        </CardContent>
-    </Card>;
-}
+export const GemCard = ({ gem }: GemCardProps) => {
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardContent className={classes.cardContent}>
+        <Typography variant="h4" component="h4">
+          <Heart gem={gem}></Heart>
+          {gem.name}
+        </Typography>
+        <hr />
+        <Typography>
+          <b>Current Version:</b> {gem.version}
+        </Typography>
+        <Typography>
+          <b>Authors:</b> {gem.authors}
+        </Typography>
+      </CardContent>
+      <ExpansionPanel>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>
+            <b>More Details</b>
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>{gem.info}</Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </Card>
+  );
+};
